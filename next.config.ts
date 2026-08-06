d4e05bb2cf7a8a0d6c02041817f185d816b1c058
Igor <maksim.kirsanov27@gmail.com>
2026-08-07 00:28:16 +0300

    Redirect www to primary domain

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,16 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: projectRoot,
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.bezdna-bar.ru" }],
+        destination: "https://bezdna-bar.ru/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
