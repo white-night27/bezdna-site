a161faa42c2d071ac1aaec532dc7182831d8d1d8
white-night27 <maksim.kirsanov27@gmail.com>
2026-09-17 01:54:49 +0300

    Point sitemap to beznabar.ru

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://bezdna-bar.ru";
+const siteUrl = "https://beznabar.ru";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
