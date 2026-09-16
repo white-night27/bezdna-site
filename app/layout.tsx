51cafed686e9aae68007445c2888ed504231c5ee
white-night27 <maksim.kirsanov27@gmail.com>
2026-09-17 02:54:29 +0300

    Restore correct production domain bezdna-bar.ru

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-const siteUrl = "https://beznabar.ru";
+const siteUrl = "https://bezdna-bar.ru";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
