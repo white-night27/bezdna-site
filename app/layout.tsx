25e4a4510ab9388aab5dcc1ab0874fd2256d99d0
white-night27 <maksim.kirsanov27@gmail.com>
2026-09-17 01:54:35 +0300

    Use beznabar.ru as canonical domain

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-const siteUrl = "https://bezdna-bar.ru";
+const siteUrl = "https://beznabar.ru";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
